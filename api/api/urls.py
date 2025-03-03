86838979f580a77a29ab7ba413c76f2a4fc4ea5b
RudovichNazar23 <n.rudovich2005@gmail.com>
2025-03-03 11:02:56 +0100

    added jwt auth

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user_profile/', include('user_profile.urls'))
+    path('user_profile/', include('user_profile.urls')),
+    path('auth/token/pair/', TokenObtainPairView.as_view(), name='token_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
